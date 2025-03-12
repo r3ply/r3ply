a253b09511dde29de4fc49e7a3a577458f26035a
Spencer Scorcelletti <contact@spenc.es>
2025-03-12 13:46:01 +0100

    Add tera templating wasm functionality

--- crates/r3ply-wasm/src/lib.rs
+++ crates/r3ply-wasm/src/lib.rs
@@ -13,18 +13,14 @@ mod tests {
     }
 }
 use wasm_bindgen::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use ammonia::Builder;
 
 #[wasm_bindgen]
 pub fn sanitize_html(text: String, allowed_tags: Vec<String>) -> String {
-    // Convert Vec<String> into HashSet<&str>
     let tag_set: HashSet<&str> = allowed_tags.iter().map(String::as_str).collect();
-
     let mut binding = Builder::default();
     let cleaner = binding.tags(tag_set);
-
-    // Example sanitized output (replace with actual input later)
     cleaner.clean(&text).to_string()
 }
 
@@ -33,8 +29,6 @@ use pulldown_cmark::{Parser, html};
 #[wasm_bindgen]
 pub fn md_to_html(text: String) -> String {
   let parser = Parser::new(&text);
-
-  // Write to a new String buffer.
   let mut html_output = String::new();
   html::push_html(&mut html_output, parser);
   return html_output
@@ -55,6 +49,25 @@ pub fn parse_email_bytes(email: &[u8]) -> JsValue {
 
 #[wasm_bindgen]
 pub fn parse_email_str(email: &str) -> JsValue {
-  // Convert the string into bytes (assumed UTF-8) and call the other function.
   parse_email_bytes(email.as_bytes())
+}
+
+use html_escape::decode_html_entities;
+
+#[wasm_bindgen]
+pub fn unescape_html(input: &str) -> String {
+  decode_html_entities(input).into_owned()
+}
+
+use tera::{Tera, Context};
+
+#[wasm_bindgen]
+pub fn tera(template: String, data: JsValue) -> Result<String, JsError> {
+  let obj: HashMap<String, serde_json::Value> = serde_wasm_bindgen::from_value(data).expect("Expected a JSON object");
+  let mut ctx = Context::new();
+  for (key, value) in obj {
+    ctx.insert(key, &value);
+  };
+  let mut tera = Tera::default();
+  tera.render_str(&template, &ctx).map_err(|error| JsError::from(error))
 }
