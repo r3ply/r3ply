d60078f5a7e2f660db2c0a2099b3f479a2d64ffd
Spencer Scorcelletti <contact@spenc.es>
2025-03-14 23:57:48 +0100

    Add a vitest workspace file to the monorepo root

--- packages/cli/test/cmd.test.ts
+++ packages/cli/test/cmd.test.ts
@@ -4,6 +4,7 @@ import { config_cmd } from '../src/cmd'
 import { Result } from 'oxide.ts'
 import mockfs from 'mock-fs'
 import fs from 'fs'
+import path from 'path'
 
 describe('config', () => {
   describe('validate', () => {
@@ -11,13 +12,13 @@ describe('config', () => {
       mockfs.restore()
     })
     test('valid r3ply config', async () => {
-      mockfs({ '/.r3ply': {}, '/r3ply.config.toml': fs.readFileSync('test/resources/minimum_r3ply_site_config.toml').toString() })
+      mockfs({ '/.r3ply': {}, '/r3ply.config.toml': fs.readFileSync(path.join(__dirname, 'resources/minimum_r3ply_site_config.toml')).toString() })
       const program = new Command()
       const result = await Result.safe(program.addCommand(config_cmd('/')).parseAsync(['node', 'test', 'config', 'validate']))
       expect((await result).isOk()).toBe(true)
     })
     test('--config option', async () => {
-      mockfs({ project: { src: {}, '.r3ply': {}, 'r3ply.config.toml': fs.readFileSync('test/resources/minimum_r3ply_site_config.toml').toString() }})
+      mockfs({ project: { src: {}, '.r3ply': {}, 'r3ply.config.toml': fs.readFileSync(path.join(__dirname, 'resources/minimum_r3ply_site_config.toml')).toString() }})
       const program = new Command()
       const result = await Result.safe(program.addCommand(config_cmd('project/src')).parseAsync(['node', 'test', 'config', 'validate', '--config', '../r3ply.config.toml']))
       expect((await result).isOk()).toBe(true)
