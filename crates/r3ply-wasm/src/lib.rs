d6608251ec45fae38e6596df071931484f8558fa
Spencer Scorcelletti <contact@spenc.es>
2025-03-14 18:23:51 +0100

    Add some extra run scripts for wasm crate + rearrange the tests

--- crates/r3ply-wasm/src/lib.rs
+++ crates/r3ply-wasm/src/lib.rs
@@ -1,22 +1,14 @@
-// TODO: add tests
-// pub fn add(left: u64, right: u64) -> u64 {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
-
 use wasm_bindgen::prelude::*;
+use serde::Serialize;
+use mail_parser::MessageParser;
+use mail_builder::{headers::{address::{Address, EmailAddress}, message_id::MessageId}, MessageBuilder};
+use std::{borrow::Cow, collections::{HashMap, HashSet}};
 use ammonia::Builder;
+use pulldown_cmark::{Parser, html};
+use html_escape::decode_html_entities;
+use tera::{Tera, Context};
 
+// html sanitization -----------------------------------------------------------
 #[wasm_bindgen]
 pub fn sanitize_html(text: String, allowed_tags: Vec<String>) -> String {
     let tag_set: HashSet<&str> = allowed_tags.iter().map(String::as_str).collect();
@@ -25,8 +17,7 @@ pub fn sanitize_html(text: String, allowed_tags: Vec<String>) -> String {
     cleaner.clean(&text).to_string()
 }
 
-use pulldown_cmark::{Parser, html};
-
+// md -> html ------------------------------------------------------------------
 #[wasm_bindgen]
 pub fn md_to_html(text: String) -> String {
   let parser = Parser::new(&text);
@@ -35,9 +26,7 @@ pub fn md_to_html(text: String) -> String {
   return html_output
 }
 
-use mail_parser::MessageParser;
-use serde::Serialize;
-
+// email parsing/building ------------------------------------------------------
 #[wasm_bindgen]
 pub fn parse_email_bytes(email: &[u8]) -> JsValue {
   let serializer = serde_wasm_bindgen::Serializer::json_compatible()
@@ -52,29 +41,6 @@ pub fn parse_email_bytes(email: &[u8]) -> JsValue {
 pub fn parse_email_str(email: &str) -> JsValue {
   parse_email_bytes(email.as_bytes())
 }
-
-use html_escape::decode_html_entities;
-
-#[wasm_bindgen]
-pub fn unescape_html(input: &str) -> String {
-  decode_html_entities(input).into_owned()
-}
-
-use tera::{Tera, Context};
-
-#[wasm_bindgen]
-pub fn tera(template: String, data: JsValue) -> Result<String, JsError> {
-  let obj: HashMap<String, serde_json::Value> = serde_wasm_bindgen::from_value(data).expect("Expected a JSON object");
-  let mut ctx = Context::new();
-  for (key, value) in obj {
-    ctx.insert(key, &value);
-  };
-  let mut tera = Tera::default();
-  tera.render_str(&template, &ctx).map_err(|error| JsError::from(error))
-}
-
-use mail_builder::{headers::{address::{Address, EmailAddress}, message_id::MessageId}, MessageBuilder};
-use std::{borrow::Cow, collections::{HashMap, HashSet}};
 
 #[wasm_bindgen]
 pub fn build_email(message_id: &str, date: Option<i64>, from_name: Option<String>, from_email: &str, to_email: &str, subject: &str, text_body: &str) -> Result<String, JsError> {
@@ -108,6 +74,25 @@ pub fn build_email(message_id: &str, date: Option<i64>, from_name: Option<String
   return email.write_to_string().map_err(|error| JsError::from(error))
 }
 
+// html decoding ---------------------------------------------------------------
+#[wasm_bindgen]
+pub fn unescape_html(input: &str) -> String {
+  decode_html_entities(input).into_owned()
+}
+
+// template processing ---------------------------------------------------------
+#[wasm_bindgen]
+pub fn tera(template: String, data: JsValue) -> Result<String, JsError> {
+  let obj: HashMap<String, serde_json::Value> = serde_wasm_bindgen::from_value(data).expect("Expected a JSON object");
+  let mut ctx = Context::new();
+  for (key, value) in obj {
+    ctx.insert(key, &value);
+  };
+  let mut tera = Tera::default();
+  tera.render_str(&template, &ctx).map_err(|error| JsError::from(error))
+}
+
+// tests -----------------------------------------------------------------------
 #[cfg(test)]
 mod tests {
     use super::*;
