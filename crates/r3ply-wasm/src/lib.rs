0ccfb4ede2aa1817f42f2b9f57195f65cd153040
Spencer Scorcelletti <contact@spenc.es>
2025-03-14 15:53:24 +0100

    Use Result for wasm's build_email + add test

--- crates/r3ply-wasm/src/lib.rs
+++ crates/r3ply-wasm/src/lib.rs
@@ -77,31 +77,60 @@ use mail_builder::{headers::{address::{Address, EmailAddress}, message_id::Messa
 use std::{borrow::Cow, collections::{HashMap, HashSet}};
 
 #[wasm_bindgen]
-pub fn build_email(message_id: String, from_name: Option<String>, from_email: String, to_email: String, subject: String, text_body: String) -> String {
-  // let mut email = MessageBuilder::new()
-  let email = MessageBuilder::new()
+pub fn build_email(message_id: &str, date: Option<i64>, from_name: Option<String>, from_email: &str, to_email: &str, subject: &str, text_body: &str) -> Result<String, JsError> {
+  let mut email = MessageBuilder::new()
     .message_id(MessageId::new(message_id))
     .from(Address::Address(EmailAddress {
       name: from_name.map(Cow::Owned),
-      email: Cow::Owned(from_email),
+      email: Cow::Borrowed(from_email),
     }))
     .to(to_email)
     .subject(subject)
     .text_body(text_body);
 
+  if let Some(date) = date {
+    email = email.date(date);
+  }
+
     // TODO: if I need to add attachments
     // if let Some(attachment) = attachment {
     //   email = email.attachment((*attachment.content_type).clone(), (*attachment.filename).clone(), (*attachment.value).clone());
     // }
+    //
+    // #[wasm_bindgen]
+    // #[derive(Clone, Debug)]
+    // pub struct Attachment {
+    //     content_type: Box<String>, // Wrapped in Box
+    //     filename: Box<String>,     // Wrapped in Box
+    //     value: Box<Vec<u8>>,       // Wrapped in Box
+    // }
 
-  return email.write_to_string().unwrap()
+  return email.write_to_string().map_err(|error| JsError::from(error))
 }
 
-// TODO: if I need to add attachments
-// #[wasm_bindgen]
-// #[derive(Clone, Debug)]
-// pub struct Attachment {
-//     content_type: Box<String>, // Wrapped in Box
-//     filename: Box<String>,     // Wrapped in Box
-//     value: Box<Vec<u8>>,       // Wrapped in Box
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_email_works() {
+      let actual = build_email("123", Some(1741963670), Some("Steve".to_owned()), "steve@apple.com", "woz@apple.com", "pokemon", "Gotta catch them all").unwrap();
+      let expected = r#"
+        Message-ID: <123>
+        From: "Steve" <steve@apple.com>
+        To: <woz@apple.com>
+        Subject: pokemon
+        Date: Fri, 14 Mar 2025 14:47:50 +0000
+        MIME-Version: 1.0
+        Content-Type: text/plain; charset="utf-8"
+        Content-Transfer-Encoding: 7bit
+
+        Gotta catch them all"#
+        .trim_start()
+        .lines()
+        .map(|line| line.trim_start()) // Remove leading spaces for each line
+        .collect::<Vec<_>>()
+        .join("\r\n");
+      assert_eq!(actual, expected );
+    }
+}
