880040cc5a570b3afca16275e5187f9913cae555
Spencer Scorcelletti <contact@spenc.es>
2025-08-31 00:00:20 +0200

    pretty print schemas

--- packages/config/src/codegen/generate.ts
+++ packages/config/src/codegen/generate.ts
@@ -5,29 +5,30 @@ import { raw_system_parser, system_schema } from '../schema/r3ply'
 // prepare directory
 mkdirSync('./src/generated', { recursive: true })
 
-// read in site template and substitute dependencies
+// read in site templates and substitute dependencies
 const site_template = readFileSync('./src/codegen/site.ts')
   .toString()
   .replace(`'<RAW_SITE_PARSER_MODULE>'`, raw_site_parser.toModule())
-
-// read in system template and substitute dependencies
 const system_template = readFileSync('./src/codegen/r3ply.ts')
   .toString()
   .replace(`'<RAW_SYSTEM_PARSER_MODULE>'`, raw_system_parser.toModule())
 
-// write rendered template to target directory, to be compiled next
+// write rendered templates to target directory, to be compiled next
 writeFileSync('./src/generated/site.ts', `// @ts-nocheck\n${site_template}`)
-writeFileSync('./src/generated/site.v0.0.1.json', JSON.stringify(site_schema))
-
-// write rendered template to target directory, to be compiled next
 writeFileSync('./src/generated/r3ply.ts', `// @ts-nocheck\n${system_template}`)
+
+// write JSON schemas to target directory, to be copied over
+writeFileSync(
+  './src/generated/site.v0.0.1.json',
+  JSON.stringify(site_schema, null, 2),
+)
 writeFileSync(
   './src/generated/r3ply.v0.0.1.json',
-  JSON.stringify(system_schema),
+  JSON.stringify(system_schema, null, 2),
 )
 
 // export everything with an index file
 writeFileSync(
   './src/generated/index.ts',
-  readFileSync('./src/codegen/index.ts').toString(),
+  readFileSync('./src/codegen/index.ts'),
 )
