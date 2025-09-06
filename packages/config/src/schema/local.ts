27f2e605f4bcff591b09cc8b6ececb4b90fc2f17
Spencer Scorcelletti <contact@spenc.es>
2025-09-06 07:56:59 +0200

    Add file_path_{} to local moderation

--- packages/config/src/schema/local.ts
+++ packages/config/src/schema/local.ts
@@ -5,10 +5,17 @@ export const local = {
   $id: 'https://r3ply.com/schemas/v0.0.1/config/local.v0.0.1.json',
   $schema: 'http://json-schema.org/draft-04/schema#',
   type: 'object',
-  required: ['type'],
+  required: ['type', 'file_path_{}'],
   additionalProperties: false,
   properties: {
     type: { const: 'local' },
+    'file_path_{}': {
+      type: 'string',
+      description: 'File path template of new comment.',
+      format: 'uri-reference',
+      examples: ['content/comments/{{ comment.id | slice(end=8) }}.md'],
+      $comment: 'Template string. Can never begin with a `/`.',
+    },
     enabled: {
       type: 'boolean',
       description: 'If false, comment is not sent for moderation.',
