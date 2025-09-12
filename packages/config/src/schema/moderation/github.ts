73c6af90f6af7ffc92bf1d64d3450a5f3d1d9131
Spencer Scorcelletti <contact@spenc.es>
2025-09-12 14:13:07 +0200

    Add title + description for github moderation

--- packages/config/src/schema/moderation/github.ts
+++ packages/config/src/schema/moderation/github.ts
@@ -4,43 +4,53 @@ import { FromSchema, JSONSchema } from 'json-schema-to-ts'
 export const github = {
   $id: 'https://r3ply.com/schemas/v0.0.1/config/moderation/github.v0.0.1.json',
   $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Github moderation',
+  description: 'Specify how comments should be sent to GitHub for moderation.',
   type: 'object',
   required: ['owner', 'repo', 'file_path_{}'],
   unevaluatedProperties: false,
   properties: {
     owner: {
+      title: 'Repo owner',
+      description: 'This should be the user or org name.',
       type: 'string',
-      description: 'Name of GitHub owner.',
       pattern: '^[\\S]+$',
       maxLength: 1024,
-      examples: ['asimpletune'],
+      examples: ['asimpletune', 'r3ply'],
     },
     repo: {
-      type: 'string',
+      title: 'Repo name',
       description: 'Name of GitHub repository.',
+      type: 'string',
       pattern: '^[\\S]+$',
       maxLength: 1024,
       examples: ['yoursite'],
     },
     'file_path_{}': {
+      title: 'File path template (string)',
+      description:
+        'Specifies the file path of the new comment. The "file_path_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'File path template of new comment.',
       pattern: '^(?!\\s*/)[\\s\\S]*$',
       maxLength: 1024,
       examples: ['content/comments/{{ comment.id | slice(end=8) }}.md'],
       $comment: 'Template string. Can never begin with a `/`.',
     },
     'base_branch_{}': {
+      title: 'Base branch template (string)',
+      description:
+        'Specifies the base branch of the new comment. The "base_branch_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Name of the base branch.',
       pattern: '^[\\s\\S]*$',
       maxLength: 128,
       default: 'main',
       $comment: 'Template string.',
     },
     'head_branch_{}': {
+      title: 'Head branch template (string)',
+      description:
+        'Specifies the head branch of the new comment. The "head_branch_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Name of head branch.',
       pattern: '^[\\s\\S]*$',
       maxLength: 256,
       default: 'comment-{{ comment.ts_rcvd }}-{{ comment.id[:8] }}.md',
@@ -48,8 +58,10 @@ export const github = {
       $comment: 'Template string.',
     },
     'commit_msg_{}': {
+      title: 'Commit message template (string)',
+      description:
+        'Specifies the commit message of the new comment. The "commit_msg_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Commit message template.',
       pattern: '^[\\s\\S]*$',
       maxLength: 2096,
       default: `Comment submitted:
@@ -60,14 +72,18 @@ Comment: > {{ comment.txt | split(pat="\n") | join(sep="> ") }}`,
       $comment: 'Template string.',
     },
     '&commit_msg_{}': {
+      title: 'Commit message template (file)',
+      description:
+        'Specifies the commit message of the new comment. The "&commit_msg_{}" name means the string will be interpreted as reference to a file that contains a string template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Commit message template.',
       format: 'uri-reference',
       $comment: 'This is a file reference.',
     },
     'pr_title_{}': {
+      title: 'PR title template (string)',
+      description:
+        'Specifies the PR title of the new comment. The "pr_title_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Pull request title template.',
       pattern: '^[\\s\\S]*$',
       maxLength: 1024,
       default:
@@ -75,23 +91,28 @@ Comment: > {{ comment.txt | split(pat="\n") | join(sep="> ") }}`,
       $comment: 'Template string.',
     },
     'pr_body_{}': {
+      title: 'PR body template (string)',
+      description:
+        'Specifies the PR body of the new comment. The "pr_body_{}" name means the string will be interpreted as a template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Pull request body template.',
       pattern: '^[\\s\\S]*$',
       maxLength: 2096,
       default: '',
       $comment: 'Template string.',
     },
     '&pr_body_{}': {
+      title: 'PR body (file)',
+      description:
+        'Specifies the PR body of the new comment. The "&pr_body_{}" name means the string will be interpreted as reference to a file that contains a string template. Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
       type: 'string',
-      description: 'Pull request body template.',
       format: 'uri-reference',
       $comment: 'File reference.',
     },
     github_host: {
+      title: 'Github host',
+      description: 'Only useful for enterprise instances of GitHub.',
       type: 'string',
       format: 'hostname',
-      description: 'The hostname of the github service.',
       default: 'github.com',
       $comment: "You probable don't want to change this.",
     },
