faf17228c0eeb7ac1103fbdd8b3df9c72f6742d4
Spencer Scorcelletti <contact@spenc.es>
2025-09-12 14:31:21 +0200

    Add method and title/description to webhook moderation

--- packages/config/src/schema/moderation/webhook.ts
+++ packages/config/src/schema/moderation/webhook.ts
@@ -2,6 +2,8 @@ import { Schema } from '@exodus/schemasafe'
 import { FromSchema, JSONSchema } from 'json-schema-to-ts'
 
 export const webhook = {
+  title: 'Webhook moderation',
+  description: 'Webhook moderation is useful for doing custom things.',
   $id: 'https://r3ply.com/schemas/v0.0.1/config/moderation/webhook.v0.0.1.json',
   $schema: 'http://json-schema.org/draft-04/schema#',
   type: 'object',
@@ -9,9 +11,21 @@ export const webhook = {
   unevaluatedProperties: false,
   properties: {
     url: {
+      title: 'Webhook URL',
+      description:
+        'The URL the comment will be sent to. The comment will be in the request body.',
       type: 'string',
       format: 'uri',
-      description: 'URL of the webhook.',
+      $comment:
+        'TODO when r3ply supports secrets allow secrets to be stored here.',
+    },
+    method: {
+      title: 'Webhook method',
+      description:
+        "The method the comment will be sent with. Default is 'POST'.",
+      type: 'string',
+      enum: ['POST', 'PUT', 'PATCH', 'DELETE'],
+      default: 'POST',
     },
   },
 } as const satisfies JSONSchema & Schema
