6c914ece0dbd0fd61eff6ef1fcd4c598bee54be9
Spencer Scorcelletti <contact@spenc.es>
2025-09-13 09:38:09 +0200

    Correctly export the schema directory

--- packages/config/src/codegen/index.ts
+++ packages/config/src/codegen/index.ts
@@ -1,11 +1,1 @@
-export { R3plySiteConfig, site_parser } from './site'
-export { R3plySignetConfig } from '../schema/signet'
-export { R3plyCommentsConfig } from '../schema/comments'
-export {
-  R3plyModerationConfig,
-  R3plyGithubConfig,
-  R3plyWebhookConfig,
-  R3plyLocalModerationConfig,
-} from '../schema/moderation'
-export { R3plyNotifyConfig } from '../schema/notify'
-export { R3plySystemConfig, system_parser } from './r3ply'
+export * from '../schema'
