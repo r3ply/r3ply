5f842f2f05f90d387c3c3c70b0f17e1f08672e27
Spencer Scorcelletti <contact@spenc.es>
2025-09-19 12:11:55 +0200

    Put back moderation options in moderation's definition key, so the requireValidation parser option can remain strict (before, when options was its own file and schema, unevaluatedOptions must be set, which prevents useing allOf, since their mutual unevaluatedOptions exclude one another. It could have been possible to set the unevaluatedProperties in their accortding to the schemas of the moderation types, but that felt overkill for hardly any benefit in return.)

--- packages/config/src/schema/moderation/local.ts
+++ packages/config/src/schema/moderation/local.ts
@@ -1,6 +1,5 @@
 import { Schema } from '@exodus/schemasafe'
-import { FromSchema, JSONSchema } from 'json-schema-to-ts'
-import { options } from './options'
+import { JSONSchema } from 'json-schema-to-ts'
 
 export const local = {
   title: 'Local moderation',
@@ -9,31 +8,18 @@ export const local = {
   $id: 'https://r3ply.com/schemas/v0.0.1/config/moderation/local.v0.0.1.json',
   $schema: 'http://json-schema.org/draft-04/schema#',
   type: 'object',
-  allOf: [
-    {
-      $ref: 'https://r3ply.com/schemas/v0.0.1/config/moderation/options.v0.0.1.json',
+  required: ['file_path_{}'],
+  unevaluatedProperties: false,
+  properties: {
+    'file_path_{}': {
+      title: 'File path template (string)',
+      description:
+        'Specifies the file path of the new comment. The "file_path_{}" name means the string will be interpreted as a template. It can never begin with a "/". Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
+      type: 'string',
+      pattern: '^(?!\\s*/)[\\s\\S]*$',
+      maxLength: 1024,
+      examples: ['content/comments/{{ comment.id | slice(end=8) }}.md'],
+      $comment: 'Template string. Can never begin with a `/`.',
     },
-    {
-      required: ['file_path_{}'],
-      unevaluatedProperties: false,
-      properties: {
-        'file_path_{}': {
-          title: 'File path template (string)',
-          description:
-            'Specifies the file path of the new comment. The "file_path_{}" name means the string will be interpreted as a template. It can never begin with a "/". Tera 2 is the templating engine. See the r3ply or tera docs for more info.',
-          type: 'string',
-          pattern: '^(?!\\s*/)[\\s\\S]*$',
-          maxLength: 1024,
-          examples: ['content/comments/{{ comment.id | slice(end=8) }}.md'],
-          $comment: 'Template string. Can never begin with a `/`.',
-        },
-      },
-    },
-  ],
+  },
 } as const satisfies JSONSchema & Schema
-export type R3plyLocalModerationConfig = FromSchema<
-  typeof local,
-  {
-    references: [typeof local, typeof options]
-  }
->
--- packages/config/src/schema/moderation/webhook.ts
+++ packages/config/src/schema/moderation/webhook.ts
@@ -1,45 +1,31 @@
 import { Schema } from '@exodus/schemasafe'
-import { FromSchema, JSONSchema } from 'json-schema-to-ts'
-import { options } from './options'
+import { JSONSchema } from 'json-schema-to-ts'
 
 export const webhook = {
-  title: 'Webhook moderation',
-  description: 'Webhook moderation is useful for doing custom things.',
   $id: 'https://r3ply.com/schemas/v0.0.1/config/moderation/webhook.v0.0.1.json',
   $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Webhook moderation',
+  description: 'Webhook moderation is useful for doing custom things.',
   type: 'object',
-  allOf: [
-    {
-      $ref: 'https://r3ply.com/schemas/v0.0.1/config/moderation/options.v0.0.1.json',
+  required: ['url'],
+  unevaluatedProperties: false,
+  properties: {
+    url: {
+      title: 'Webhook URL',
+      description:
+        'The URL the comment will be sent to. The comment will be in the request body.',
+      type: 'string',
+      format: 'uri',
+      $comment:
+        'TODO when r3ply supports secrets allow secrets to be stored here.',
     },
-    {
-      required: ['url'],
-      unevaluatedProperties: false,
-      properties: {
-        url: {
-          title: 'Webhook URL',
-          description:
-            'The URL the comment will be sent to. The comment will be in the request body.',
-          type: 'string',
-          format: 'uri',
-          $comment:
-            'TODO when r3ply supports secrets allow secrets to be stored here.',
-        },
-        method: {
-          title: 'Webhook method',
-          description:
-            "The method the comment will be sent with. Default is 'POST'.",
-          type: 'string',
-          enum: ['POST', 'PUT', 'PATCH', 'DELETE'],
-          default: 'POST',
-        },
-      },
+    method: {
+      title: 'Webhook method',
+      description:
+        "The method the comment will be sent with. Default is 'POST'.",
+      type: 'string',
+      enum: ['POST', 'PUT', 'PATCH', 'DELETE'],
+      default: 'POST',
     },
-  ],
+  },
 } as const satisfies JSONSchema & Schema
-export type R3plyWebhookConfig = FromSchema<
-  typeof webhook,
-  {
-    references: [typeof webhook, typeof options]
-  }
->
