fc841fdf0d84f0f675793c51a46aa3b0243b42a3
Spencer Scorcelletti <contact@spenc.es>
2025-10-06 11:03:40 +0200

    Make sure to copy all schemas to dist during build process

---
+++ packages/schema/src/codegen/copy_schemas.ts
@@ -0,0 +1,37 @@
+import { extra, signet, site } from '../config'
+import { comments, email } from '../config/comments'
+import { moderation, github, local, webhook } from '../config/moderation'
+import { notify } from '../config/notify'
+import path from 'path'
+import fs from 'fs'
+
+console.log('Copying schemas to dist...\n')
+
+const cwd = process.cwd()
+const dist_dir = 'dist'
+
+/**
+ * all schemas have to be added here to be copied over
+ */
+const schemas = [
+  site,
+  extra,
+  signet,
+  comments,
+  email,
+  moderation,
+  github,
+  webhook,
+  local,
+  notify,
+]
+
+for (const s of schemas as { $id: string }[]) {
+  const url = new URL(s.$id)
+  const joined = path.join(cwd, dist_dir, url.pathname)
+  fs.mkdirSync(path.dirname(joined), { recursive: true })
+  fs.writeFileSync(joined, JSON.stringify(s, null, 2))
+  console.log(`Wrote ${joined}`)
+}
+
+console.log('\nFinished writing schemas.')
--- packages/schema/src/codegen/generate.ts
+++ packages/schema/src/codegen/generate.ts
@@ -1,10 +1,5 @@
 import { writeFileSync, mkdirSync, readFileSync } from 'fs'
-import {
-  raw_site_parser,
-  site_schema,
-  raw_system_parser,
-  system_schema,
-} from '../config'
+import { raw_site_parser, raw_system_parser } from '../config'
 
 // prepare directory
 mkdirSync('./src/generated', { recursive: true })
@@ -20,16 +15,6 @@ const system_template = readFileSync('./src/codegen/r3ply.ts')
 // write rendered templates to target directory, to be compiled next
 writeFileSync('./src/generated/site.ts', `// @ts-nocheck\n${site_template}`)
 writeFileSync('./src/generated/r3ply.ts', `// @ts-nocheck\n${system_template}`)
-
-// write JSON schemas to target directory, to be copied over
-writeFileSync(
-  './src/generated/site.v0.0.1.json',
-  JSON.stringify(site_schema, null, 2),
-)
-writeFileSync(
-  './src/generated/r3ply.v0.0.1.json',
-  JSON.stringify(system_schema, null, 2),
-)
 
 // export everything with an index file
 writeFileSync(
