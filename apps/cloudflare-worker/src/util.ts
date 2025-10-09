55acc9c865030ed89924f8e092b70aff744631e2
Spencer Scorcelletti <contact@spenc.es>
2025-10-09 14:08:19 +0200

    Update d1 to have a simple 'comments' table - along with a comments_via_email table - and have inserting into that table return metadata + begin integrating r3ply library changes to the cloudflare worker

--- apps/cloudflare-worker/src/util.ts
+++ apps/cloudflare-worker/src/util.ts
@@ -1,3 +1,5 @@
+import { util } from '@r3ply/lib'
+
 /**
  * Use this to curry dependencies.
  * e.g. with `OmitFirstParameter<typeof update_gist>` then:
@@ -11,3 +13,82 @@ export type OmitFirstParameter<T extends (...args: any) => any> = T extends (
 ) => infer R
   ? (...args: P) => R
   : never
+
+/**
+ * Dereference a file located at a URL relative to a base URL
+ *
+ * @param base_url
+ * @param file_uri_ref
+ *
+ * @returns the contents of the file or undefined
+ */
+export const DereferenceFileAtURL: util.config.DerferenceFile = async (
+  base_url: string,
+  file_uri_ref?: string,
+) => {
+  if (file_uri_ref) {
+    return fetch(
+      url_path_relative_to_base(
+        file_uri_ref,
+        new URL(
+          base_url.startsWith('https://') ? base_url : 'https://' + base_url,
+        ),
+      ),
+    ).then((response) => response.text())
+  } else {
+    return Promise.resolve(undefined)
+  }
+}
+
+function url_path_relative_to_base(path: string, base: URL) {
+  if (path.startsWith('/')) return new URL(path, base)
+  else return new URL('./' + path, base)
+}
+
+if (import.meta.vitest) {
+  const { test, expect, describe } = import.meta.vitest
+
+  describe('url path relative to base', () => {
+    const config_url = new URL(
+      'https://example.com/.well-known/r3ply/config.toml',
+    )
+    const test_fn = url_path_relative_to_base
+    test('file path local to base', () => {
+      expect(test_fn('file.txt', config_url).pathname).toBe(
+        '/.well-known/r3ply/file.txt',
+      )
+    })
+    test('file path nested under base', () => {
+      expect(test_fn('a/b/c/file.txt', config_url).pathname).toBe(
+        '/.well-known/r3ply/a/b/c/file.txt',
+      )
+    })
+    test('file path above base', () => {
+      expect(test_fn('../file.txt', config_url).pathname).toBe(
+        '/.well-known/file.txt',
+      )
+    })
+    test('file path sibling of base', () => {
+      expect(test_fn('../sibling/file.txt', config_url).pathname).toBe(
+        '/.well-known/sibling/file.txt',
+      )
+    })
+    test('file path relative to root', () => {
+      expect(test_fn('/a/b/c/file.txt', config_url).pathname).toBe(
+        '/a/b/c/file.txt',
+      )
+    })
+    test('file path is a URL', () => {
+      const url = test_fn('https://evil.com/a/b/c/file.txt', config_url)
+      expect(url.pathname).toBe(
+        '/.well-known/r3ply/https://evil.com/a/b/c/file.txt',
+      )
+      expect(url.hostname).toBe(config_url.hostname)
+    })
+    test('file path has a hostname', () => {
+      const url = test_fn('evil.com/a/b/c/file.txt', config_url)
+      expect(url.pathname).toBe('/.well-known/r3ply/evil.com/a/b/c/file.txt')
+      expect(url.hostname).toBe(config_url.hostname)
+    })
+  })
+}
