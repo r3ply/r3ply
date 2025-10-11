619927fc0952b5f25cf0650adc91f65a516298e6
Spencer Scorcelletti <contact@spenc.es>
2025-10-11 09:38:22 +0200

    Rename comment generation dir for CLI and add some retries when generating text

---
+++ apps/cli/src/comment_generation/train.ts
@@ -0,0 +1,61 @@
+import { RiTa } from 'rita'
+import fs from 'fs'
+
+// Note: most of these are commented out because at the moment small models seem to be good enough but are also much more performant
+// TODO: in the future I should spend more time filtering and preprocessing the data.
+const input_files = [
+  './src/comment_generation/training_data/hackernews_parents_10000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_10500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_11000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_11500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_12000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_12500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_13000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_13500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_14000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_14500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_15500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_16000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_16500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_17000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_17500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_18000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_18500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_19000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_19500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_20000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_20500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_21000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_21500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_22000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_22500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_23000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_23500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_24000000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_24500000.comments.json',
+  // './src/comment_generation/training_data/hackernews_parents_25000000.comments.json'
+]
+
+// Initialize the model with length 5 n-gram
+const markov = RiTa.markov(5)
+
+// Read the file and add each comment to the model
+function train_on_file(input_file_path: string) {
+  console.log(`processing file: ${input_file_path}`)
+  const comments: string[] = JSON.parse(
+    fs.readFileSync(input_file_path).toString(),
+  )
+  markov.addText(comments)
+}
+
+// Iterate through each file and train on it
+console.log('Beginning training...')
+input_files.forEach(train_on_file)
+
+// Save the trained model to a file
+console.log('writing training model to file')
+fs.writeFileSync(
+  'src/comment_generation/comments-markov-model.json',
+  markov.toJSON(),
+)
+console.log('Model saved to src/comment_generation/comments-markov-model.json')
--- apps/cli/tsup.config.js
+++ apps/cli/tsup.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: 'esm',
-  onSuccess: 'cp src/comments/comments-markov-model.json dist/ || true',
+  onSuccess:
+    'cp src/comment_generation/comments-markov-model.json dist/ || true',
   dts: true,
   clean: false,
   target: 'node20',
