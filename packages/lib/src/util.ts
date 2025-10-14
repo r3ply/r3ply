e12fc9abab82dfebf2c4b13da2c5089ea3b50112
Spencer Scorcelletti <contact@spenc.es>
2025-10-14 09:08:36 +0200

    Remove stale comments in lib/util

--- packages/lib/src/util.ts
+++ packages/lib/src/util.ts
@@ -71,25 +71,28 @@ export namespace config {
     if (template.uri) return dereference(base_uri, template.uri)
     else return template.str
   }
-
-  if (import.meta.vitest) {
-    const { test, expect } = import.meta.vitest
-    test('resolve config references 2', async () => {
-      let count = 1
-      let config = {
-        a: 'CHANGE ME',
-        '&a': 'foo',
-        b: { '&cat': 'cat', ca: 'bar', cat2: 'dog' },
-      }
-      const resolved = await resolve_references(config, '', async (uri, ref) =>
-        (count++).toString(),
-      )
-      expect(resolved).toStrictEqual({
-        a: '1',
-        '&a': 'foo',
-        b: { '&cat': 'cat', ca: 'bar', cat2: 'dog', cat: '2' },
+  namespace resolve_config_reference {
+    if (import.meta.vitest) {
+      const { test, expect } = import.meta.vitest
+      test('resolve_config_reference', async () => {
+        let count = 1
+        let config = {
+          a: 'CHANGE ME',
+          '&a': 'foo',
+          b: { '&cat': 'cat', ca: 'bar', cat2: 'dog' },
+        }
+        const resolved = await resolve_references(
+          config,
+          '',
+          async (uri, ref) => (count++).toString(),
+        )
+        expect(resolved).toStrictEqual({
+          a: '1',
+          '&a': 'foo',
+          b: { '&cat': 'cat', ca: 'bar', cat2: 'dog', cat: '2' },
+        })
       })
-    })
+    }
   }
 }
 
@@ -119,72 +122,3 @@ export function base64UrlDecode(str: string): Uint8Array {
   const binary = atob(str)
   return new Uint8Array([...binary].map((c) => c.charCodeAt(0)))
 }
-
-// TODO: uncomment
-// TESTS
-// if (import.meta.vitest) {
-//   const { it, expect } = import.meta.vitest
-//   it('resolves remote references to templates', async () => {
-//     const parsed_config = siteConfigParser(
-//       JSON.stringify({
-//         version: '0.0.1',
-//         site: [
-//           {
-//             domain: 'spenc.es',
-//             r3ply: 'r3ply.com',
-//             signet: 'qhQ6YSUvQNLb1lCdw3kDRg',
-//             issued: '2025-08-22',
-//           },
-//         ],
-//         comments: {
-//           email: {
-//             enabled: false,
-//             moderation: {
-//               type: 'github',
-//               repo: 'https://github.com/example/your-repo',
-//               'file_path_{}': '{{ comment.id }}.txt',
-//               '&commit_msg_{}': '/commit/msg.txt',
-//               '&pr_body_{}': '../pr/body.txt',
-//             },
-//             '&comment_{}': '/comment/template/path.txt',
-//             'comment_{}_mime': '',
-//             notify: {
-//               '&comment_received_notif_{}': '/comment/received.txt',
-//               '&comment_submitted_notif_{}': '/comment/submitted.txt',
-//             },
-//           },
-//         },
-//         enabled: false,
-//       }),
-//     )
-
-//     const config: R3plySiteConfig = parsed_config.value!
-//     const dereference: DerferenceFile = async (_, file_uri_ref?: string) => {
-//       if (file_uri_ref) return `File Contents of '${file_uri_ref}'`
-//       else return undefined
-//     }
-//     const resolved_config = await resolve_config_references(
-//       config,
-//       'https://example.com',
-//       dereference,
-//     )
-//     expect(resolved_config.comments.email['comment_{}']).toBe(
-//       "File Contents of '/comment/template/path.txt'",
-//     )
-//     expect(
-//       resolved_config.comments.email.notify['comment_received_notif_{}'],
-//     ).toBe("File Contents of '/comment/received.txt'")
-//     expect(
-//       resolved_config.comments.email.notify['comment_submitted_notif_{}'],
-//     ).toBe("File Contents of '/comment/submitted.txt'")
-//     expect(resolved_config.comments.email.moderation.type).toBe('github')
-//     if (resolved_config.comments.email.moderation.type == 'github') {
-//       expect(resolved_config.comments.email.moderation['commit_msg_{}']).toBe(
-//         "File Contents of '/commit/msg.txt'",
-//       )
-//       expect(resolved_config.comments.email.moderation['pr_body_{}']).toBe(
-//         "File Contents of '../pr/body.txt'",
-//       )
-//     }
-//   })
-// }
